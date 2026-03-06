75c64da6ecd977e6d58d0ef30262f8e66faea42d
Miller Daniel <millerdanielsousaaa@gmail.com>
2026-03-06 12:14:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4624054547-e60b8",
+  "appId": "1:488781387039:web:5c16b524983cf8f05ccc2a",
+  "apiKey": "AIzaSyA0OsuP6B4rn-J7buayEvR30HigLQ2tHJI",
+  "authDomain": "studio-4624054547-e60b8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "488781387039"
+};
