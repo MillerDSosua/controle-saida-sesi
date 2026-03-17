c84d0f63feae78429adeae8fa215f594c3d265f7
Miller Daniel <millerdanielsousaaa@gmail.com>
2026-03-17 10:11:57 -0300

    fix next config standalone

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,37 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  // 🔥 importante para Docker
   output: "standalone",
   outputFileTracingRoot: path.join(__dirname),
+
+  // ✔ manter seus ajustes
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
